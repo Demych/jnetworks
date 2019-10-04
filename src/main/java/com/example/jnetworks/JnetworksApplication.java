43243d8f3550117bb460f80dc6787d3a63c6e945
aldz0219 <aliaksandr.dzemchanka@netcracker.com>
2019-10-04 19:49:01 +0300

    project init

---
+++ src/main/java/com/example/jnetworks/JnetworksApplication.java
@@ -0,0 +1,13 @@
+package com.example.jnetworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JnetworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JnetworksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jnetworks/JnetworksApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.jnetworks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JnetworksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
